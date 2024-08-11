1e75efdc5f0ce8bfb238f803ee7dff183934001b
Alex Zenger <alexander.zenger@1komma5grad.com>
2024-08-11 22:02:09 +0200

    adds examples for differences between stages

--- expert/variables.tf
+++ expert/variables.tf
@@ -13,3 +13,17 @@ variable "run_max_scale" {
 variable "db_tier" {
   type = string
 }
+
+variable "toggle_feat_xy" {
+  type    = bool
+  default = false
+}
+
+variable "additional_pubsub_subscriptions" {
+  type = map(object({
+    name          = string
+    topic         = string
+    push_endpoint = string
+  }))
+  default = {}
+}
